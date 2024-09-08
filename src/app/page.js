7190ef8477340709e25efcd0373f9a97e0aa5def
asr <ankitasinharay3@gmail.com>
2024-09-09 03:08:28 +0530

    add error and loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Results from "@/components/Results";
 
+
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
@@ -8,7 +9,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    // { next: { revalidate: 10000 } }
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
